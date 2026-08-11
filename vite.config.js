bc43c60225a9775d0106c8b7ccb2a95c7d013bd3
Sai Vinoth <sairio274@gmail.com>
2026-08-11 14:11:14 +0530

    Fix broken dependencies from npm audit fix --force

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,8 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: false // Add sourcemap
+        sourcemap: false, // Add sourcemap
+        target: 'esnext' // Modern browsers only (required for top-level-await plugin)
     },
     plugins:
     [
